9a0b99fa473ff865019e0fb1f29a7815f20f34d3
Jose Victor  Reis Portal <josevitorr666@gmail.com>
2026-03-15 18:11:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function sitevitrine() {
+    window.open(' https://josevictortech.github.io/site-vitrine/' , '_blank')
+}
+function list() {
+    window.open(' https://josevictortech.github.io/todo-list-javascript/', '_blank')
+}
+function idade(){
+    window.open(' https://josevictortech.github.io/age-checker-javascript/' ,'_blank')
+}
